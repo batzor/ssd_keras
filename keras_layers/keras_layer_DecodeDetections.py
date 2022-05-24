de3b30d987bdf8485673378e21e7ac0193648b60
batzor <32958247+batzor@users.noreply.github.com>
2022-05-24 22:15:11 +0900

    Fix imports

--- keras_layers/keras_layer_DecodeDetections.py
+++ keras_layers/keras_layer_DecodeDetections.py
@@ -21,8 +21,7 @@ from __future__ import division
 import numpy as np
 import tensorflow as tf
 import keras.backend as K
-from keras.engine.topology import InputSpec
-from keras.engine.topology import Layer
+from tensorflow.keras.engine.topology import InputSpec, Layer
 
 class DecodeDetections(Layer):
     '''
