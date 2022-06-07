daf7ec460330e68c8c44ffc11d48018a889e2ae3
batzor <32958247+batzor@users.noreply.github.com>
2022-06-07 12:56:00 +0900

    Fix generator

--- data_generator/object_detection_2d_data_generator.py
+++ data_generator/object_detection_2d_data_generator.py
@@ -1171,6 +1171,7 @@ class DataGenerator:
             if 'original_images' in returns: ret.append(batch_original_images)
             if 'original_labels' in returns: ret.append(batch_original_labels)
 
+            ret = tuple(e for e in ret)
             yield ret
 
     def save_dataset(self,
